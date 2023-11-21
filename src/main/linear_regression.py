6e938ae947167fd9128533e088317fcc81132887
zkac246 <zkac246@live.rhul.ac.uk>
2023-11-21 22:07:58 +0000

    feat: added LinearRegression class constructor

---
+++ src/main/linear_regression.py
@@ -0,0 +1,6 @@
+class LinearRegressionClassifier():
+    """Class representing a linear regression classifier"""
+
+    def __init__(self, ):
+        self.X_train = None
+        self.y_train = None
