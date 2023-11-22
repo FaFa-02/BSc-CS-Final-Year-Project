05c18997e4d1c56e9d42e0bd1c5179ddfffdd234
zkac246 <zkac246@live.rhul.ac.uk>
2023-11-22 21:48:06 +0000

    feat: fit() method implemented to fit data and label sets to classifier

--- src/main/linear_regression.py
+++ src/main/linear_regression.py
@@ -2,6 +2,12 @@
 class LinearRegressionClassifier():
     """Class representing a linear regression classifier"""
 
-    def __init__(self, ):
+    def __init__(self):
         self.X_train = None
         self.y_train = None
+
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    
--- src/test/test_linear_regression.py
+++ src/test/test_linear_regression.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import numpy as np
 
 sys.path.append('..')
 
@@ -10,9 +11,13 @@ class TestLinRegClassFit(unittest.TestCase):
 
     def setUp(self):
         self.lin_reg = LinearRegressionClassifier()
+        X_train = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+        y_train = np.array([1, 2])
+        self.lin_reg.fit(X_train, y_train)
 
-    def test_linReg_fit(self):
+    def test_lin_reg_fit(self):
         self.assertIsNotNone(self.lin_reg.X_train)
+        self.assertIsNotNone(self.lin_reg.y_train)
 
 if __name__ == "__main__":
     unittest.main()
