34917d9ccb36bcf2fe3dcc661173187a365373d2
zkac246 <zkac246@live.rhul.ac.uk>
2023-11-22 13:44:56 +0000

    Revert "feat: added LinearRegression class constructor", should have been created in a new feature branch
    This reverts commit f3764b68e3e4b0fd878abc134207f4eaaa9eb3c2.

--- src/main/linear_regression.py
+++
@@ -1,6 +0,0 @@
-class LinearRegressionClassifier():
-    """Class representing a linear regression classifier"""
-
-    def __init__(self, ):
-        self.X_train = None
-        self.y_train = None
