9f22ba296ec87b11f536b22dbbfc1d0d9f2a5150
zkac246 <zkac246@live.rhul.ac.uk>
2023-12-06 15:02:08 +0000

    test: added test for predict() method

--- src/test/test_linear_regression.py
+++ src/test/test_linear_regression.py
@@ -13,11 +13,16 @@ class TestLinRegClassFit(unittest.TestCase):
         self.lin_reg = LinearRegressionClassifier()
         X_train = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
         y_train = np.array([1, 2])
+        test_dataset = np.array([[1, 2, 3, 4]])
         self.lin_reg.fit(X_train, y_train)
 
     def test_lin_reg_fit(self):
         self.assertIsNotNone(self.lin_reg.X_train)
         self.assertIsNotNone(self.lin_reg.y_train)
+
+    def test_lin_reg_predict(self):
+        self.assertIsNotNone(self.lin_reg.predict(self.test_dataset))
+
 
 if __name__ == "__main__":
     unittest.main()
