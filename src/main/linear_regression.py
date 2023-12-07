c15739bbede3ea8e5ddc18cf280721ab700d67d5
zkac246 <zkac246@live.rhul.ac.uk>
2023-12-07 05:50:47 +0000

    feat: added coefficient optimisation to regression class

--- src/main/linear_regression.py
+++ src/main/linear_regression.py
@@ -1,13 +1,22 @@
+import numpy as np
+from numpy.linalg import inv
+
 """Module providing a linear regression classifier with appropriate functions"""
 class LinearRegressionClassifier():
     """Class representing a linear regression classifier"""
 
-    def __init__(self):
+    def __init__(self, penalty):
         self.X_train = None
         self.y_train = None
+        self.penalty = penalty
 
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
-    
-    
+
+        self.m = X_train.shape[0] 
+        self.n = X_train.shape[1]
+
+        I = np.identity(self.m)
+
+        beta_ridge = ((inv((self.X_train.T).dot(self.X_train) + self.penalty * I)).dot(self.X_train.T)).dot(y_train)
