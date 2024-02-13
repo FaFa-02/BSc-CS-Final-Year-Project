e8c8aa69e5c4214fe583487ef91a72e98673cd43
zkac246 <zkac246@live.rhul.ac.uk>
2024-02-13 15:07:08 +0000

    docs: added comments to gui

--- src/main/model_gui.py
+++ src/main/model_gui.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 
+#initialises the application on menu page
 root = tk.Tk()
 root.title("Model Menu")
 root.eval("tk::PlaceWindow . center")
 
+#C
 frame1 = tk.Frame(root, width=500, height=600, bg="#fff")
 frame1.grid(row=0, column=0)
 
