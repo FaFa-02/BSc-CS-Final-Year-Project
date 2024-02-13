e9012c736355363dbe0de4c55a02b377b4963324
zkac246 <zkac246@live.rhul.ac.uk>
2024-02-13 16:16:19 +0000

    refactor: encapsulated load frame code in a method

--- src/main/model_gui.py
+++ src/main/model_gui.py
@@ -2,6 +2,26 @@ import tkinter as tk
 
 bg_colour = "#fff"
 
+def load_menu():
+    frame1.pack_propagate(False)
+
+    # frame1 widgets
+    tk.Label(frame1,
+            text="Ridge & Lasso Regression Menu",
+            bg=bg_colour,
+            fg="black",
+            font=("TkMenuFont", 14)
+            ).pack()
+
+    tk.Button(frame1,
+            text="Proof of Concept",
+            font=("TkMenuFont", 20),
+            bg=bg_colour,
+            fg="black",
+            cursor="hand2",
+            command=lambda:load_POC()
+            ).pack()
+
 # Loads POC menu frame (only test for now)
 def load_POC():
     print("test")
@@ -14,23 +34,5 @@ root.eval("tk::PlaceWindow . center")
 # Create frame widget
 frame1 = tk.Frame(root, width=500, height=600, bg=bg_colour)
 frame1.grid(row=0, column=0)
-frame1.pack_propagate(False)
-
-# frame1 widgets
-tk.Label(frame1,
-         text="Ridge & Lasso Regression Menu",
-         bg=bg_colour,
-         fg="black",
-         font=("TkMenuFont", 14)
-         ).pack()
-
-tk.Button(frame1,
-          text="Proof of Concept",
-          font=("TkMenuFont", 20),
-          bg=bg_colour,
-          fg="black",
-          cursor="hand2",
-          command=lambda:load_POC()
-          ).pack()
 
 root.mainloop()
