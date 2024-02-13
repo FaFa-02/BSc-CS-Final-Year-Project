cfcc728e44a7870d150b16dd68cbe5dc0109dcbe
zkac246 <zkac246@live.rhul.ac.uk>
2024-02-13 19:41:06 +0000

    docs: added documentation to main.py

--- src/main/main.py
+++ src/main/main.py
@@ -1,18 +1,20 @@
+"""Module initialising program application with appropriate user functions"""
 import tkinter as tk
 
-bg_colour = "#fff"
+BG_COLOUR = "#fff"
 
 class Menu:
+    """Class representing the main menu window"""
     def __init__(self, parent):
         self.parent = parent
-        self.menu = tk.Frame(self.parent, width=500, height=600, bg=bg_colour)
+        self.menu = tk.Frame(self.parent, width=500, height=600, bg=BG_COLOUR)
         self.menu.grid(row=0, column=0)
         self.menu.pack_propagate(False)
 
         # menu frame widgets
         tk.Label(self.menu,
                 text="Ridge & Lasso Regression Menu",
-                bg=bg_colour,
+                bg=BG_COLOUR,
                 fg="black",
                 font=("TkMenuFont", 14)
                 ).pack()
@@ -20,7 +22,7 @@ class Menu:
         tk.Button(self.menu,
                 text="Proof of Concept",
                 font=("TkMenuFont", 20),
-                bg=bg_colour,
+                bg=BG_COLOUR,
                 fg="black",
                 cursor="hand2",
                 command=lambda:load_poc(self)
@@ -31,13 +33,15 @@ class Menu:
             self.app = Poc(self.newWindow)
 
 class Poc:
+    """Class representing the proof of concept program window"""
     def __init__(self, parent):
         self.parent = parent
-        self.poc = tk.Frame(self.parent, width=500, height=600, bg=bg_colour)
+        self.poc = tk.Frame(self.parent, width=500, height=600, bg=BG_COLOUR)
         self.poc.grid(row=0, column=0)
         self.poc.pack_propagate(False)
 
 def main():
+    """Class representing the root window"""
     # Initialises the application on menu page
     root = tk.Tk()
     app = Menu(root)
