cfc1e26b98c27977fd5231bd39628e65dec061a7
zkac246 <zkac246@live.rhul.ac.uk>
2024-04-05 01:59:18 +0100

    feat: created knn regression class with constructor

---
+++ src/main/k_nearest_neighbors.py
@@ -0,0 +1,6 @@
+"""Module providing a K Nearest Neighbour Regression model with appropriate functions"""
+class KNearestNeighbors():
+    """Class representing a KNN regression model"""
+
+    def __init__(self, n = 3):
+        self.n = n
